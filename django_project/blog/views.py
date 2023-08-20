979935846852884aa98a7a17a9e032263bafc691
KellyDanielO <kizdver@gmail.com>
2023-08-20 19:36:26 +0100

    .

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 14, 2023'
+    },
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 20, 2023'
+    },
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
